bc38490f4cf2c2007b863954e16e3857b63d50c6
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2018-07-22 15:55:09 +0200

    cleanup compartment

--- freezerapp/frontend/src/app/freezer/compartment/compartment.component.ts
+++ freezerapp/frontend/src/app/freezer/compartment/compartment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Compartment } from './compartment.model';
 import { FreezerDataService } from '../freezer-data.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Freezer } from '../freezer/freezer.model';
 
 @Component({
   selector: 'app-compartment',
@@ -11,6 +12,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class CompartmentComponent implements OnInit {
   public errorMsg: string;
   @Input() public compartment: Compartment;
+  @Input() public freezer: Freezer;
   private _size: number;
   
   @Output() public deleteCompartment = new EventEmitter<Compartment>();
